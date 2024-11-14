49ef7c2e71f457f4b81c6348edac7d9041416b38
Neha Lad <neha.lad@neom.com>
2024-11-14 16:40:52 +0530

    Delete ADW directory

--- ADW/readme.txt
+++
@@ -1,1 +0,0 @@
-Add all type of SQL's here...!!!!!
--- ADW/v1.0.2/emp.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE employees (
-    employee_id NUMBER PRIMARY KEY,  -- lint warning: primary key should be defined as 'NOT NULL'
-    first_name VARCHAR2(50),  -- lint warning: column names should be snake_case
-    last_name VARCHAR2(50),
-    hire_date DATE DEFAULT SYSDATE,
-    salary NUMBER
-);
